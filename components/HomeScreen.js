f7b7e6b14e3082f2479ef3f73295a3b63ca04098
hawkilol <abacates21@gmail.com>
2023-11-08 09:22:21 -0300

    testing: routing test

---
+++ components/HomeScreen.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View , Text, StyleSheet, Button } from 'react-native'
+import MyComponent from './MyComponent';
+import { useNavigation } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import NotHomeScreen from './NotHomeScreen';
+
+function HomeScreen({ navigation }) {
+    // const navigation = useNavigation();
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Text>Meu ™!</Text>
+        <MyComponent/>
+        <Button
+            title="Go to Not Home"
+            onPress={() => navigation.navigate('NotHome')}
+      />
+      </View>
+    );
+}
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Meu ™!</Text>
+//       <StatusBar style="auto" />
+//       <MyComponent/>
+//     </View>
+//   );
+// }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+export default HomeScreen;
---
+++ components/NotHomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View , Text } from 'react-native'
+
+function NotHomeScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Not Home Screen</Text>
+      </View>
+    );
+}
+
+export default NotHomeScreen;
