ee524b81fb76d7f4507361928c4538a3d91c9bfc
Kalil <67834605+hawkilol@users.noreply.github.com>
2023-11-16 14:56:17 -0300

    Update models.py

--- backend/quickstart/models.py
+++ backend/quickstart/models.py
@@ -2,16 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
+# Create your models here.
+
 class Ranking(models.Model):
     Updated = models.DateField()
     RankingID = models.CharField(max_length=255)
     Name = models.CharField(max_length=128)
     RankingType = models.CharField(max_length=64)
     Gender = models.CharField(max_length=32)
-    RankingItems = models.ManyToManyField('RankingItems')
+    RankingItems = models.ForeignKey('RankingItem', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.Name
+    
 
 class RankingItem(models.Model):
     Type = models.CharField(max_length=32)
@@ -20,6 +25,7 @@ class RankingItem(models.Model):
     Rank = models.IntegerField(max_length=255)
     RankingItemsCode = models.IntegerField(max_length=255)
     Person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    Ranking = models.ForeignKey('Ranking', on_delete=models.CASCADE)
 
 class Person(models.Model):
     Updated = models.DateField()
